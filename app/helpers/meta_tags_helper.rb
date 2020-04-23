691577f4f21edf8f08e6f3fc9c022381af453e67
guillaume crouillere <gcrouillere@gmail.com>
2020-04-23 14:37:06 +0200

    change main model

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:title_tag) ? content_for(:title_tag) : "Les chapeaux uniques créés par Dominique Chapre"
+    content_for?(:title_tag) ? content_for(:title_tag) : "Les créations textiles uniques par Dominique Chapre"
   end
 
   def meta_product_name
-    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les chapeaux uniques créés par Dominique Chapre - vente de produits de l'artisanat"
+    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les créations textiles uniques par Dominique Chapre - vente de produits de l'artisanat"
   end
 
   def meta_description
-    description = "Des produits extraordinaires. Décor somptueux. Venez découvrir leur fabrication lors d'un stage."
+    description = "Découvrez toutes les créations textiles : chapeaux, foulards, chemises en teintures naturelles, en vente dans la boutique. Vous pouvez également réserver un stage pour vous initier où vous perfectionner dans cet artisanat d'art unique."
     content_for?(:description) ? content_for(:description) : description
   end
 
