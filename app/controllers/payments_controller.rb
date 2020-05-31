cfd9c5435c6872b881bddd98f217ba851124e208
guillaume crouillere <gcrouillere@gmail.com>
2020-05-31 11:19:43 +0200

    add unknown order state

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -37,11 +37,13 @@ class PaymentsController < ApplicationController
     payment_intent = Stripe::PaymentIntent.retrieve(session["payment_intent"])
     status = payment_intent["status"]
 
+    # Check other statuses fro stripe : WIP ?
     if status == "succeeded"
       document_order_basketlines
       @order.update(stripe_payment_intent: payment_intent, state: 'paid', method: 'stripe')
       sends_mails_after_order
     else
+      @order.update(stripe_payment_intent: payment_intent, state: 'unkown', method: 'stripe')
       flash[:notice] = t(:payment_failure)
       redirect_to new_order_payment_url(@order)
     end
